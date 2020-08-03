6abfc3762b9e3044173cebe2ea1c1ac344b324ce
Gilvando Lucas Vieira <gilvando@pop-os.localdomain>
2020-08-03 15:41:17 -0300

    First commit

---
+++ api/src/test/java/vieira/gilvando/api/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package vieira.gilvando.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
